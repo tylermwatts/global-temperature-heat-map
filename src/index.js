9c5b756e82d93ea05ee6829183259a8c64207eca
warpfox <tylermwatts@gmail.com>
2018-12-06 21:30:05 -0600

    shit finally works, need to add tooltip & legend

--- src/index.js
+++ src/index.js
@@ -6,72 +6,94 @@ req.open("GET", "/src/data.json", true);
 req.send();
 req.onload = function() {
   const json = JSON.parse(req.responseText);
-  const w = 1410;
-  const h = 600;
-  const padding = 50;
+  const w = 1430;
+  const h = 480;
+  const padding = 60;
   const baseTemp = json.baseTemperature;
   const xScale = d3
-    .scaleTime()
+    .scaleLinear()
     .domain([
-      d3.min(
-        json.monthlyVariance.map(d =>
-          new Date().setFullYear(d.year)
-        )
-      ),
-      d3.max(
-        json.monthlyVariance.map(d =>
-          new Date().setFullYear(d.year)
-        )
-      )
+      d3.min(json.monthlyVariance.map(d => d.year)),
+      d3.max(json.monthlyVariance.map(d => d.year))
     ])
-    .range([padding, w - padding]);
+    .range([padding, w]);
   const yScale = d3
-    .scaleTime()
-    .domain([
-      new Date().setMonth(0),
-      new Date().setMonth(11)
-    ])
+    .scaleBand()
+    .domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     .range([h - padding, padding]);
-  const xAxis = d3
-    .axisBottom(xScale)
-    .ticks(26)
-    .tickFormat(d3.timeFormat("%Y"));
-  const yAxis = d3
-    .axisLeft(yScale)
-    .tickFormat(d3.timeFormat("%b"));
+  const xAxis = d3.axisBottom(xScale).tickFormat(d => d.toString());
+  const yAxis = d3.axisLeft(yScale).tickFormat(function(d) {
+    switch (d) {
+      case 0:
+        return "January";
+      case 1:
+        return "February";
+      case 2:
+        return "March";
+      case 3:
+        return "April";
+      case 4:
+        return "May";
+      case 5:
+        return "June";
+      case 6:
+        return "July";
+      case 7:
+        return "August";
+      case 8:
+        return "September";
+      case 9:
+        return "October";
+      case 10:
+        return "November";
+      case 11:
+        return "December";
+      default:
+        return "";
+    }
+  });
   const svg = d3
     .select("body")
     .append("svg")
-    .attr("width", w + padding)
-    .attr("height", h + padding);
+    .attr("width", w)
+    .attr("height", h);
   svg
     .selectAll("rect")
     .data(json.monthlyVariance)
     .enter()
     .append("rect")
     .attr("class", "cell")
-    .attr("x", d =>
-      xScale(new Date().setFullYear(d.year))
-    )
-    .attr(
-      "y",
-      d =>
-        h - yScale(new Date().setMonth(d.month - 1))
-    )
+    .attr("x", d => xScale(d.year))
+    .attr("y", d => yScale(d.month - 1))
     .attr("data-month", d => d.month - 1)
     .attr("data-year", d => d.year)
-    .attr("data-temp", d => baseTemp + d.variance);
+    .attr("data-temp", d => baseTemp + d.variance)
+    .style("fill", d => {
+      let temp = baseTemp + d.variance;
+      if (temp <= 5) {
+        return "blue";
+      }
+      if (temp > 5 && temp <= 7) {
+        return "aqua";
+      }
+      if (temp > 7 && temp < 8) {
+        return "bisque";
+      }
+      if (temp >= 8 && temp <= 10) {
+        return "orange";
+      }
+      if (temp > 10) {
+        return "red";
+      }
+    });
   svg
     .append("g")
-    .attr(
-      "transform",
-      "translate(0," + (h - padding) + ")"
-    )
+    .attr("transform", "translate(0," + (h - padding) + ")")
     .call(xAxis)
     .attr("id", "x-axis");
   svg
     .append("g")
-    .attr("transform", "translate(50,0)")
+    .attr("transform", "translate(" + padding + ",0)")
     .call(yAxis)
     .attr("id", "y-axis");
 };
